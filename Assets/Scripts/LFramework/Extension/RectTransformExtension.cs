b6ad797d21def37e106cf28acb27f77b15b9008a
刘浩文 <597094538@qq.com>
2021-11-19 23:11:39 +0800

    添加RectTransform扩展类

---
+++ Assets/Scripts/LFramework/Extension/RectTransformExtension.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// RectTransform扩展类
+/// </summary>
+public static class RectTransformExtension 
+{
+    /// <summary>
+    /// 设置Rect大小
+    /// </summary>
+    public static void SetRectTransformSize(this RectTransform rt, Vector2 size)
+    {
+        Vector2 oldSize = rt.rect.size;
+        Vector2 deltaSize = size - oldSize;
+        rt.offsetMin = rt.offsetMin - new Vector2(deltaSize.x * rt.pivot.x, deltaSize.y * rt.pivot.y);
+        rt.offsetMax = rt.offsetMax + new Vector2(deltaSize.x * (1f - rt.pivot.x), deltaSize.y * (1f - rt.pivot.y));
+    }
+}
