7ab23ab25dd09c605be99b2511a9d8e67fb964e2
刘浩文 <597094538@qq.com>
2021-12-05 18:33:35 +0800

    添加UI工具：是否点击到了UI

---
+++ Assets/Scripts/LFramework/Utils/UIUtils.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// UI工具类
+/// </summary>
+public static class UIUtils
+{
+    /// <summary>
+    /// 是否点击到了UI
+    /// </summary>
+    public static bool IsPointOverUI()
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = Input.mousePosition;
+
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
+        return results.Count > 0;
+    }
+}
