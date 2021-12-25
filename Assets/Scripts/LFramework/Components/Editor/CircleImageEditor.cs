1b41cf42557c4979c687b98b6703021a8075bd8a
刘浩文 <597094538@qq.com>
2021-12-25 18:22:36 +0800

    添加圆形Image组件相关

---
+++ Assets/Scripts/LFramework/Components/Editor/CircleImageEditor.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CircleImage))]
+[CanEditMultipleObjects]
+public class CircleImageEditor : Editor
+{
+    SerializedProperty m_Sprite;
+    SerializedProperty m_Color;
+    SerializedProperty m_RaycastTarget;
+    SerializedProperty m_RenderType;
+    SerializedProperty m_FilledType;
+    SerializedProperty m_Origin360;
+    SerializedProperty m_Clockwise;
+    SerializedProperty m_FillAmount;
+
+    private void OnEnable()
+    {
+        m_Sprite = serializedObject.FindProperty("m_Sprite");
+        m_Color= serializedObject.FindProperty("m_Color");
+        m_RaycastTarget = serializedObject.FindProperty("m_RaycastTarget");
+        m_RenderType = serializedObject.FindProperty("m_RenderType");
+        m_FilledType = serializedObject.FindProperty("m_FilledType");
+        m_Origin360 = serializedObject.FindProperty("m_Origin360");
+        m_Clockwise = serializedObject.FindProperty("m_Clockwise");
+        m_FillAmount = serializedObject.FindProperty("m_FillAmount");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(m_Sprite);
+        EditorGUILayout.PropertyField(m_Color);
+        EditorGUILayout.PropertyField(m_RaycastTarget);
+        EditorGUILayout.PropertyField(m_RenderType);
+        if ((CircleImage.RenderType)m_RenderType.intValue == CircleImage.RenderType.Simple)
+        {
+
+        }
+        else if ((CircleImage.RenderType)m_RenderType.intValue == CircleImage.RenderType.Filled)
+        {
+            EditorGUILayout.PropertyField(m_FilledType);
+            if ((CircleImage.FilledType)m_FilledType.intValue == CircleImage.FilledType.Radial360)
+            {
+                EditorGUILayout.PropertyField(m_Origin360);
+            }
+            EditorGUILayout.PropertyField(m_Clockwise);
+            EditorGUILayout.PropertyField(m_FillAmount);
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ Assets/Scripts/LFramework/Components/Editor/CreateUIEditor.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIEditor
+{
+    const int UI_LAYER = 5;
+
+    [MenuItem("GameObject/LFramework/UI/CircleImage", priority = 10)]
+    public static void CreateCircleImage()
+    {
+        GameObject obj = InitUIComponent("CircleImage");
+        CircleImage component = obj.AddComponent<CircleImage>();
+        component.raycastTarget = false;
+    }
+
+    /// <summary>
+    /// UI组件初始化
+    /// </summary>
+    static GameObject InitUIComponent(string componentName)
+    {
+        Transform canvasTrans = GetCanvasRoot();
+
+        GameObject obj = new GameObject(componentName);
+        obj.layer = UI_LAYER;
+        obj.AddComponent<RectTransform>();
+        if (Selection.activeGameObject != null
+            && Selection.activeGameObject.layer == UI_LAYER)
+        {
+            obj.transform.SetParent(Selection.activeGameObject.transform);
+        }
+        else
+        {
+            obj.transform.SetParent(canvasTrans);
+        }
+        obj.transform.localPosition = Vector3.zero;
+
+        return obj;
+    }
+
+    /// <summary>
+    /// 获取UI画布
+    /// </summary>
+    static Transform GetCanvasRoot()
+    {
+        Canvas canvas = GameObject.FindObjectOfType<Canvas>();
+        if (canvas == null)
+        {
+            GameObject canvasObj = new GameObject("Canvas");
+            canvasObj.layer = UI_LAYER;
+            canvasObj.AddComponent<RectTransform>();
+            canvasObj.AddComponent<Canvas>().renderMode = RenderMode.ScreenSpaceOverlay;
+            canvasObj.AddComponent<CanvasScaler>();
+            canvasObj.AddComponent<GraphicRaycaster>();
+            return canvasObj.transform;
+        }
+        else
+        {
+            return canvas.transform;
+        }
+    }
+}
