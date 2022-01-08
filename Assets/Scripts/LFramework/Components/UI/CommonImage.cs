9da386fb976b52fdbbc8baf748401976d6e55cf8
刘浩文 <597094538@qq.com>
2022-01-08 22:20:19 +0800

    添加普通Image组件相关

---
+++ Assets/Scripts/LFramework/Components/UI/CommonImage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 普通Image组件
+/// </summary>
+[AddComponentMenu("LFramework/UI/CommonImage", 10)]
+public class CommonImage : Image
+{
+
+}
