f1a0284d7f49671bd52c1708976673ad669d3c6b
刘浩文 <597094538@qq.com>
2022-02-19 17:40:03 +0800

    【工具类】添加数学相关工具类

---
+++ Assets/Scripts/LFramework/Utils/MathUtils.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 数学相关工具类
+/// </summary>
+public static class MathUtils
+{
+    /// <summary>
+    /// 四舍五入
+    /// </summary>
+    /// digits:保留几位小数
+    public static float Round(float value, int digits = 1)
+    {
+        float multiple = Mathf.Pow(10, digits);
+        float tempValue = value * multiple + 0.5f;
+        tempValue = Mathf.FloorToInt(tempValue);
+        return tempValue / multiple;
+    }
+}
