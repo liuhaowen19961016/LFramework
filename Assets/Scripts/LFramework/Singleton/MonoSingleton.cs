88d100351f9646efe0ad44bfab033410ba1e97a5
刘浩文 <597094538@qq.com>
2022-03-11 23:04:03 +0800

    单例模版

---
+++ Assets/Scripts/LFramework/Singleton/MonoSingleton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Mono单例模版
+/// </summary>
+public class MonoSingleton<T> : MonoBehaviour
+    where T : MonoBehaviour
+{
+    public T _ins;
+    public T Ins
+    {
+        get
+        {
+            if (_ins == null)
+            {
+
+            }
+            return _ins;
+        }
+    }
+}
