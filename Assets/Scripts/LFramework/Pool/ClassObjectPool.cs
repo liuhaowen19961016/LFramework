ba62e9dd3c65ae890ee2ab2ef8dc15bff3f6a0c8
liuhaowen <597094538@qq.com>
2023-02-09 10:58:29 +0800

    类对象池

--- Assets/Scripts/LFramework/Pool/ClassObjectPool.cs
+++ Assets/Scripts/LFramework/Pool/ClassObjectPool.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class ClassObjectPool<T>
     where T : class, new()
 {
-    private Stack<T> m_Stack = new Stack<T>();//类栈
+    private Stack<T> m_ClassStack = new Stack<T>();//类栈
     private int m_Capacity;//容量（-1为无限容量）
     private int m_NoRecycleCount;//没有回收的数量
 
@@ -20,10 +20,9 @@ public class ClassObjectPool<T>
         for (int i = 0; i < m_Capacity; i++)
         {
             T t = new T();
-            m_Stack.Push(t);
+            m_ClassStack.Push(t);
         }
     }
-
 
     /// <summary>
     /// 从池子中取
@@ -31,22 +30,21 @@ public class ClassObjectPool<T>
     /// <param name="createNew">如果缓存栈中没有了，是否new一个</param>
     public T Allocate(bool createNew = true)
     {
-        if (m_Stack.Count <= 0)
+        T t = default;
+        if (m_ClassStack.Count <= 0)
         {
             if (createNew)
             {
-                T t = new T();
+                t = new T();
                 m_NoRecycleCount++;
-                return t;
             }
-            return null;
         }
         else
         {
-            T t = m_Stack.Pop();
+            t = m_ClassStack.Pop();
             m_NoRecycleCount++;
-            return t;
         }
+        return t;
     }
 
     /// <summary>
@@ -54,18 +52,20 @@ public class ClassObjectPool<T>
     /// </summary>
     public bool Recycle(T t)
     {
-        if (t == null)
+        bool ret = false;
+        if (t != null)
         {
-            return false;
+            m_NoRecycleCount--;
+            if (m_Capacity > 0 && m_ClassStack.Count >= m_Capacity)
+            {
+                t = null;
+            }
+            else
+            {
+                m_ClassStack.Push(t);
+                ret = true;
+            }
         }
-        m_NoRecycleCount--;
-        if (m_Stack.Count >= m_Capacity
-            && m_Capacity > 0)
-        {
-            t = null;
-            return false;
-        }
-        m_Stack.Push(t);
-        return true;
+        return ret;
     }
 }
