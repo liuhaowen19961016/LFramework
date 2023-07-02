9c21da98aae6ab3759254dc24239bc78cb6b6176
刘浩文 <597094538@qq.com>
2023-07-02 11:18:19 +0800

    优化MonoSingleton

--- Assets/Scripts/LFramework/Singleton/MonoSingleton.cs
+++ Assets/Scripts/LFramework/Singleton/MonoSingleton.cs
@@ -3,25 +3,35 @@
 public abstract class MonoSingleton<T> : MonoBehaviour
     where T : MonoBehaviour
 {
+    private static GameObject root;
+
     private static T m_Ins = null;
     public static T Ins
     {
         get
         {
+            if (root == null)
+            {
+                root = GameObject.Find("MonoSingleton");
+                if (root == null)
+                {
+                    root = new GameObject("MonoSingleton");
+                    DontDestroyOnLoad(root);
+                }
+            }
             if (m_Ins == null)
             {
                 m_Ins = FindObjectOfType(typeof(T)) as T;
+                string name = typeof(T).ToString();
                 if (m_Ins == null)
                 {
-                    m_Ins = new GameObject(typeof(T).ToString(), typeof(T)).GetComponent<T>();
-                    var root = GameObject.Find("MonoSingleton");
-                    if (root == null)
-                    {
-                        root = new GameObject("MonoSingleton");
-                        DontDestroyOnLoad(root);
-                    }
-                    m_Ins.transform.parent = root.transform;
+                    m_Ins = new GameObject(name, typeof(T)).GetComponent<T>();
                 }
+                else
+                {
+                    m_Ins.gameObject.name = name;
+                }
+                m_Ins.transform.parent = root.transform;
             }
             return m_Ins;
         }
