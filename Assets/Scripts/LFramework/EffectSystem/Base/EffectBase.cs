6ad120af5f11f86e854075c11082c5cda2e77545
liuhaowen <597094538@qq.com>
2023-07-27 18:15:19 +0800

    特效系统

---
+++ Assets/Scripts/LFramework/EffectSystem/Base/EffectBase.cs
@@ -0,0 +1,191 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 特效基类
+/// </summary>
+public abstract class EffectBase : IEffect
+{
+    private EEffectType m_EffectType;//特效类型
+    public EEffectType EffectType
+    {
+        get
+        {
+            return m_EffectType;
+        }
+    }
+
+    private GameObject m_EffectGo;//特效实体
+    public GameObject EffectGo
+    {
+        get
+        {
+            return m_EffectGo;
+        }
+    }
+
+    private EffectDefine m_EffectDefine;//特效配置
+    public EffectDefine EffectDefine
+    {
+        get
+        {
+            return m_EffectDefine;
+        }
+    }
+
+    private float m_Duration;//持续时间
+    public float Duration
+    {
+        get
+        {
+            return m_Duration;
+        }
+    }
+
+    private TimerTask m_CompleteTimerTask;
+
+    public bool Init(EffectDefine effectDefine)
+    {
+        m_EffectType = (EEffectType)effectDefine.effectType;
+        m_EffectDefine = effectDefine;
+        m_EffectGo = EffectMgr.Ins.EffectPool.GetEffectGo(effectDefine.effectPath);
+        if (m_EffectGo == null)
+        {
+            Debug.LogError("特效实体为null");
+            EffectMgr.Ins.EffectPool.PutEffect(this);
+            return false;
+        }
+        else
+        {
+            m_Duration = GetDuration(m_EffectGo.transform);
+            Debug.Log(m_Duration);
+            m_CompleteTimerTask = TimerMgr.Ins.Register(m_Duration, onComplete: () =>
+                {
+                    EffectMgr.Ins.EffectPool.PutEffect(this);
+                });
+            return true;
+        }
+    }
+
+    public void SetWorldPosition(Vector3 worldPos)
+    {
+        if (m_EffectGo == null)
+        {
+            return;
+        }
+        m_EffectGo.transform.position = worldPos;
+    }
+
+    public void SetWorldRotation(Vector3 worldRotation)
+    {
+        if (m_EffectGo == null)
+        {
+            return;
+        }
+        m_EffectGo.transform.rotation = Quaternion.Euler(worldRotation);
+    }
+
+    public void SetLocalPosition(Vector3 localPos)
+    {
+        if (m_EffectGo == null)
+        {
+            return;
+        }
+        m_EffectGo.transform.localPosition = localPos;
+    }
+
+    public void SetLocalRotation(Vector3 localRotation)
+    {
+        if (m_EffectGo == null)
+        {
+            return;
+        }
+        m_EffectGo.transform.localRotation = Quaternion.Euler(localRotation);
+    }
+
+    public void SetScale(Vector3 scale)
+    {
+        if (m_EffectGo == null)
+        {
+            return;
+        }
+        m_EffectGo.transform.localScale = scale;
+    }
+
+    public void SetSpeed(float speed)
+    {
+        if (m_EffectGo == null)
+        {
+            return;
+        }
+    }
+
+    public void SetLayer(int layer, bool inculdeAllChildren)
+    {
+        if (m_EffectGo == null)
+        {
+            return;
+        }
+    }
+
+    public void SetOrder(int order)
+    {
+        if (m_EffectGo == null)
+        {
+            return;
+        }
+        Renderer[] renderers = m_EffectGo.GetComponentsInChildren<Renderer>(true);
+        for (int i = 0; i < renderers.Length; ++i)
+        {
+            renderers[i].sortingOrder = order;
+        }
+    }
+
+    public void ChangeRoot(Transform parent)
+    {
+        if (m_EffectGo == null || parent == null)
+        {
+            return;
+        }
+        m_EffectGo.transform.SetParent(parent);
+    }
+
+    public void OnGet()
+    {
+
+    }
+
+    public void OnPut()
+    {
+        EffectMgr.Ins.EffectPool.PutEffectGo(m_EffectGo);
+        m_EffectType = EEffectType.Null;
+        m_EffectGo = null;
+        m_EffectDefine = null;
+        m_CompleteTimerTask?.Dispose();
+    }
+
+    /// <summary>
+    /// 获取持续时间
+    /// </summary>
+    public float GetDuration(Transform root)
+    {
+        //TODO
+        float duration = 0;
+        ParticleSystem rootParticleSystem = root.GetComponent<ParticleSystem>();
+        if (rootParticleSystem != null)
+        {
+            duration = Mathf.Max(duration, rootParticleSystem.main.duration);
+        }
+        foreach (Transform trans in root)
+        {
+            ParticleSystem particleSystem = trans.GetComponent<ParticleSystem>();
+            if (particleSystem == null)
+            {
+                continue;
+            }
+            float tempDuration = particleSystem.main.duration;
+            duration = Mathf.Max(duration, tempDuration);
+            duration = Mathf.Max(duration, GetDuration(trans));
+        }
+        return duration;
+    }
+}
---
+++ Assets/Scripts/LFramework/EffectSystem/Base/IEffect.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 特效接口
+/// </summary>
+public interface IEffect : IEffectAction
+{
+    EEffectType EffectType { get; }//特效类型
+    GameObject EffectGo { get; }//特效实体
+    EffectDefine EffectDefine { get; }//特效配置
+    float Duration { get; }//持续时间
+
+    /// <summary>
+    /// 设置世界坐标位置
+    /// </summary>
+    void SetWorldPosition(Vector3 worldPos);
+
+    /// <summary>
+    /// 设置世界坐标旋转
+    /// </summary>
+    void SetWorldRotation(Vector3 worldRotation);
+
+    /// <summary>
+    /// 设置局部坐标位置
+    /// </summary>
+    void SetLocalPosition(Vector3 localPos);
+
+    /// <summary>
+    /// 设置局部坐标旋转
+    /// </summary>
+    void SetLocalRotation(Vector3 localRotation);
+
+    /// <summary>
+    /// 设置缩放
+    /// </summary>
+    void SetScale(Vector3 scale);
+
+    /// <summary>
+    /// 设置速度
+    /// </summary>
+    void SetSpeed(float speed);
+
+    /// <summary>
+    /// 设置层级
+    /// </summary>
+    void SetLayer(int layer, bool inculdeAllChildren);
+
+    /// <summary>
+    /// 设置层级排序
+    /// </summary>
+    void SetOrder(int order);
+
+    /// <summary>
+    /// 改变挂点
+    /// </summary>
+    void ChangeRoot(Transform parent);
+}
+
+/// <summary>
+/// 特效事件接口
+/// </summary>
+public interface IEffectAction
+{
+    /// <summary>
+    /// 初始化（获取实体、设置参数）
+    /// </summary>
+    bool Init(EffectDefine effectDefine);
+
+    /// <summary>
+    /// 获取特效的回调
+    /// </summary>
+    void OnGet();
+
+    /// <summary>
+    /// 回收特效的回调
+    /// </summary>
+    void OnPut();
+}
+
+/// <summary>
+/// 特效类型
+/// </summary>
+public enum EEffectType
+{
+    Null,
+    Common = 1,             //通用特效
+    UI,                     //UI特效
+}
