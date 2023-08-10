af39f6d096f9f21184364ef891da5d72b5d7279a
liuhaowen <597094538@qq.com>
2023-08-10 17:43:23 +0800

    正则工具类

---
+++ Assets/Scripts/LFramework/Utils/RegexUtils.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+/// <summary>
+/// 正则工具类
+/// </summary>
+public static class RegexUtils
+{
+    /// <summary>
+    /// 是否为合法的C#命名
+    /// </summary>
+    public static bool IsValidName(string str)
+    {
+        Regex regex = new Regex(@"^[a-zA-Z_][A-Za-z0-9_]*$");
+        return regex.IsMatch(str);
+    }
+}
